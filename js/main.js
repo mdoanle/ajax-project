ca1a0cdc0e4f9b79e97ab6c563595a9b44d8e509
Markus Doan-Le <markus.doanle@gmail.com>
2022-09-26 21:50:42 +0000

    Create GIFs of the feature being used on both mobile and desktop #1

--- js/main.js
+++ js/main.js
@@ -5,12 +5,11 @@ function handleSubmit(event) {
   event.preventDefault();
   var $searchBar = document.querySelector('.user-search');
   var userSearch = $searchBar.value;
-  getCardData(userSearch);
-}
-
-function getCardData(name) {
   var xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://api.magicthegathering.io/v1/cards?name=' + name);
+  xhr.open('GET', 'https://api.magicthegathering.io/v1/cards?name=' + userSearch);
   xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+
+  });
   xhr.send();
 }
