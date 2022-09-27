c75d5879a160da07d461289e26b16cf66f14bae7
Markus Doan-Le <markus.doanle@gmail.com>
2022-09-27 22:11:17 +0000

    make sure that all click targets show a cursor:pointer when hovered by the mouse #2

--- js/main.js
+++ js/main.js
@@ -1,15 +1,63 @@
 var $form = document.querySelector('form');
+var $searchAnchor = document.querySelector('.search-anchor');
 
 $form.addEventListener('submit', handleSubmit);
 function handleSubmit(event) {
   event.preventDefault();
   var $searchBar = document.querySelector('.user-search');
   var userSearch = $searchBar.value;
+  var $cardImage = document.querySelector('.card-image');
   var xhr = new XMLHttpRequest();
   xhr.open('GET', 'https://api.magicthegathering.io/v1/cards?name=' + userSearch);
   xhr.responseType = 'json';
   xhr.addEventListener('load', function () {
-
+    var card = xhr.response.cards[0];
+    swapToCardView();
+    $cardImage.src = card.imageUrl;
+    showCardInfo(card);
   });
   xhr.send();
+}
+
+$searchAnchor.addEventListener('click', handleClick);
+function handleClick(event) {
+  if (event.target.matches('.search-anchor')) {
+    swapToSearchView();
+  }
+}
+
+function swapToSearchView() {
+  var $searchView = document.querySelector('.home-screen');
+  var $cardView = document.querySelector('.card-view');
+  $searchView.className = 'home-screen';
+  $cardView.className = 'card-view hidden';
+}
+
+function swapToCardView() {
+  var $searchView = document.querySelector('.home-screen');
+  var $cardView = document.querySelector('.card-view');
+  $cardView.className = 'card-view';
+  $searchView.className = 'home-screen hidden';
+}
+
+function showCardInfo(object) {
+  var $cardName = document.querySelector('.card-title');
+  var $manaCost = document.querySelector('.mana-cost');
+  var $cardType = document.querySelector('.card-type');
+  var $cardText = document.querySelector('.card-text');
+  var $cardMechanics = document.querySelector('.card-text-mechanics');
+  var $flavorText = document.querySelector('.flavor-text');
+  var $artistName = document.querySelector('.artist');
+  var splitCardText = object.text.split('\n');
+
+  $cardName.textContent = object.name;
+  $manaCost.textContent = object.manaCost;
+  $cardType.textContent = object.originalType;
+  $cardText.textContent = splitCardText[0];
+  $cardMechanics.textContent = splitCardText[1];
+  $flavorText.textContent = '"' + object.flavor + '"';
+  if (object.flavor == null) {
+    $flavorText.textContent = '';
+  }
+  $artistName.textContent = 'Illustrated by: ' + object.artist;
 }
