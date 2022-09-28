7fb9afc0665a2754b92abff38cd9a5b41ccf8d37
Markus Doan-Le <markus.doanle@gmail.com>
2022-09-28 16:15:24 +0000

    Create an event listener that will save the data model with all of the favorited card data into local storage #3

--- js/data.js
+++ js/data.js
@@ -3,3 +3,14 @@ var data = {
   savedCards: [],
   savedCardID: 1
 };
+
+var savedCardsJSON = localStorage.getItem('Saved Cards');
+if (savedCardsJSON != null) {
+  data = JSON.parse(savedCardsJSON);
+}
+
+addEventListener('beforeunload', handleBeforeUnload);
+function handleBeforeUnload(event) {
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('Saved Cards', dataJSON);
+}
